3362c94ffcb802c087b5cd5e5ac88b0b41942b72
chiragkyal <chirag.kyal@gmail.com>
2024-03-05 00:25:35 +0530

    plumbing secretsGetter and sarClient for ExtendedValidator

--- pkg/cmd/infra/router/template.go
+++ pkg/cmd/infra/router/template.go
@@ -723,6 +723,10 @@ func (o *TemplateRouterOptions) Run(stopCh <-chan struct{}) error {
 	if err != nil {
 		return err
 	}
+	authorizationClient, err := authorizationclient.NewForConfig(config)
+	if err != nil {
+		return err
+	}
 
 	var cfgManager templateplugin.ConfigManager
 	var blueprintPlugin router.Plugin
@@ -740,6 +744,8 @@ func (o *TemplateRouterOptions) Run(stopCh <-chan struct{}) error {
 			WildcardRoutesAllowed:     o.AllowWildcardRoutes,
 			ExtendedValidation:        o.ExtendedValidation,
 			AllowExternalCertificates: o.AllowExternalCertificates,
+			SecretsGetter:             kc.CoreV1(),
+			SarClient:                 authorizationClient.SubjectAccessReviews(),
 		}
 		cfgManager = haproxyconfigmanager.NewHAProxyConfigManager(cmopts)
 		if len(o.BlueprintRouteNamespace) > 0 {
@@ -813,7 +819,7 @@ func (o *TemplateRouterOptions) Run(stopCh <-chan struct{}) error {
 	plugin = controller.NewRouteSecretManager(plugin, recorder, secretManager)
 
 	if o.ExtendedValidation {
-		plugin = controller.NewExtendedValidator(plugin, recorder, o.AllowExternalCertificates)
+		plugin = controller.NewExtendedValidator(plugin, recorder, o.AllowExternalCertificates, kc.CoreV1(), authorizationClient.SubjectAccessReviews())
 	}
 	plugin = controller.NewUniqueHost(plugin, o.RouterSelection.DisableNamespaceOwnershipCheck, recorder)
 	plugin = controller.NewHostAdmitter(plugin, o.RouteAdmissionFunc(), o.AllowWildcardRoutes, o.RouterSelection.DisableNamespaceOwnershipCheck, recorder)
